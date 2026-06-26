74818392bcf33ca87c4ecbc77a9ebe9284bb62d0
abdulaleemarshad1979 <abdulaleemarsahdm@gmail.com>
2026-06-26 16:36:50 +0530

    Update public.profiles insert RLS policy to WITH CHECK (true)

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -22,7 +22,7 @@ CREATE POLICY "Public profiles are viewable by everyone" ON public.profiles
   FOR SELECT USING (true);
 
 CREATE POLICY "Users can insert their own profile" ON public.profiles
-  FOR INSERT WITH CHECK (auth.uid() = id);
+  FOR INSERT WITH CHECK (true);
 
 CREATE POLICY "Users can update their own profile" ON public.profiles
   FOR UPDATE USING (auth.uid() = id);
